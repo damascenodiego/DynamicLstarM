2c526cc692f8b149cc6b5a148d00a6f97158e561
damascenodiego <carlos.damasceno08@gmail.com>
2017-07-21 17:03:59 -0300

    creation of folder using timestamp as filename

--- mealyInference/plotcharts.r
+++ mealyInference/plotcharts.r
@@ -1,20 +1,25 @@
 library(ggplot2)
 library(reshape2)
 
-tab <- read.table("./output.txt", sep="\t", header=TRUE)
+tab <- read.table("./Experiment_17_07_21_13_17_05/output.txt", sep="\t", header=TRUE)
 tab<-tab[(tab$step<=70),]
+#tab<-tab[!(tab$config=="none"),]
 
 #tab<-tab[!(tab$config=="ce_cache_rev"),]
 #tab<-tab[!(tab$config=="ce_rev"),]
 
+#newdir <- paste("Experiment", format(Sys.time(),"%y_%m_%d_%H_%M_%S"), sep = "_")
+#dir.create(newdir)
+
+newdir <-'Experiment_17_07_21_13_17_05'
 
 for(metric_name in c(
                       #"rounds", 
                       "mq_resets", "mq_symbol", "eq_resets", "eq_symbol", "learning", "search_ce"
                       #,"learning_ce" 
                     )){
-  #for(scenario_name in c("fsm", "fsm_best", "fsm_mid")){
-  for(scenario_name in unique(tab$scenario)){
+  for(scenario_name in c("fsm", "fsm_best", "fsm_mid")){
+  #for(scenario_name in unique(tab$scenario)){
     tab_agg <- aggregate(tab[[metric_name]], by=list(tab$scenario, tab$config, tab$step), 
                      function(x)mean(x, na.rm=TRUE))
     names(tab_agg) <- c("scenario", "config", "step", "average")
@@ -28,16 +33,19 @@ for(metric_name in c(
                y = average, 
                group=config, 
                color=config
-               )) + geom_point() + geom_line() 
-    #plot <- plot + ggtitle(paste(metric_name, " to infer ",scenario_name,sep="")) + theme(plot.title = element_text(hjust = 0.5))
+               )) + geom_point() + geom_line() + geom_smooth(aes(x = step, y = average, group=config, color=config), method=lm, se=FALSE)
+    plot <- plot + ggtitle(paste(metric_name, " for ",scenario_name,sep="")) + theme(plot.title = element_text(hjust = 0.5))
     plot <- plot + xlab("Number of features") + ylab(paste("Avg (",metric_name,")",sep=""))
-    plot <- plot + scale_color_discrete(name="Configuration") 
+    plot <- plot + scale_color_discrete(name="Configuration", breaks=c("Default","CE","CE + Filter")) 
     plot <- plot + theme_bw()
     plot <- plot + theme(legend.position = "bottom", legend.background = element_rect(color = "black", size = 0.2, linetype = "solid"), legend.direction = "horizontal")
     #plot <- plot + theme(legend.position = "right", legend.background = element_rect(color = "black", size = 0.2, linetype = "solid"), legend.direction = "vertical")
     
-    
-    filename <- paste(metric_name,"_",scenario_name,".png",sep="")
+    #filename <- paste(newdir,"/",scenario_name,"_",metric_name,".png",sep="")
+    filename <- paste(newdir,"/",metric_name,"_",scenario_name,".png",sep="")
     ggsave(filename)
   }
 }
+
+file.copy(list.files(".","output.txt", full.names = TRUE),"newdir")
+file.copy(list.files(".",".+log", full.names = TRUE),"newdir")
