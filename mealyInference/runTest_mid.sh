c07cc1528141132f11e26aab7bde7593bc41f450
damascenodiego <carlos.damasceno08@gmail.com>
2018-03-17 14:51:32 -0300

    configuration selection using number of features but %

--- mealyInference/runTest_mid.sh
+++ mealyInference/runTest_mid.sh
@@ -4,25 +4,28 @@ source constants.sh
 
 ## declare an array variable
 arr=("experiments_mid/fsm/fsm_15_2.txt" "experiments_mid/fsm/fsm_15_576.txt" "experiments_mid/fsm/fsm_15_1152.txt");
+arr_suls=("./SULs/2feat.txt" "./SULs/50Perc.txt" "./SULs/100Perc.txt")
+mkdir -p ./SULs
 
 rm ./log4j/*.log
-rm ./experiments_mid/fsm/fsm_*.ot
-rm ./experiments_mid/fsm/fsm_*.sul
-rm ./experiments_mid/fsm/fsm_*.infer
-rm ./experiments_mid/fsm/fsm_*.final
-rm ./experiments_mid/fsm/fsm_*.reval
+rm ./SULs/*
 
 logdir=log_experiments_mid$(date +"%Y%m%d_%H%M%S_%N")
 
 ## now loop through the above array
 for a in `seq 1 $reps`; do
    arr[0]="experiments_mid/fsm/"`java -jar selectConfig.jar ./experiments_mid/fsm/configurations_fsm_15.txt 2`".txt"
-   arr[1]="experiments_mid/fsm/"`java -jar selectConfig.jar ./experiments_mid/fsm/configurations_fsm_15.txt 0.5`".txt"
-   arr[2]="experiments_mid/fsm/"`java -jar selectConfig.jar ./experiments_mid/fsm/configurations_fsm_15.txt 1.0`".txt"
-   for i in "${arr[@]}"; do
+   arr[1]="experiments_mid/fsm/"`java -jar selectConfig.jar ./experiments_mid/fsm/configurations_fsm_15.txt 576`".txt"
+   arr[2]="experiments_mid/fsm/"`java -jar selectConfig.jar ./experiments_mid/fsm/configurations_fsm_15.txt 1152`".txt"
+
+   cp ${arr[0]} ./SULs/2feat.txt
+   cp ${arr[1]} ./SULs/50Perc.txt
+   cp ${arr[2]} ./SULs/100Perc.txt
+
+   for i in "${arr_suls[@]}"; do
       echo java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
       java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
-      for j in "${arr[@]}"; do
+      for j in "${arr_suls[@]}"; do
          for b in `seq 1 $reps`; do
             java -jar ./Infer_LearnLib.jar -sul $j -ot $i.ot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
          done
@@ -52,8 +55,3 @@ sed -i "s/|\ /|/g" ./log4j/log.tab
 
 mkdir $logdir/
 mv ./log4j $logdir/
-mv ./experiments_mid/fsm/fsm_*.ot  $logdir/
-mv ./experiments_mid/fsm/fsm_*.sul  $logdir/
-mv ./experiments_mid/fsm/fsm_*.infer  $logdir/
-mv ./experiments_mid/fsm/fsm_*.final  $logdir/
-mv ./experiments_mid/fsm/fsm_*.reval  $logdir/
--- mealyInference/runTest_mid_low.sh
+++ mealyInference/runTest_mid_low.sh
@@ -4,25 +4,28 @@ source constants.sh
 
 ## declare an array variable
 arr=("experiments_mid_low/fsm/fsm_15_2.txt" "experiments_mid_low/fsm/fsm_15_176.txt" "experiments_mid_low/fsm/fsm_15_352.txt"); 
+arr_suls=("./SULs/2feat.txt" "./SULs/50Perc.txt" "./SULs/100Perc.txt")
+mkdir -p ./SULs
 
 rm ./log4j/*.log
-rm ./experiments_mid_low/fsm/fsm_*.ot
-rm ./experiments_mid_low/fsm/fsm_*.sul
-rm ./experiments_mid_low/fsm/fsm_*.infer
-rm ./experiments_mid_low/fsm/fsm_*.final
-rm ./experiments_mid_low/fsm/fsm_*.reval
+rm ./SULs/*
 
 logdir=log_experiments_mid_low$(date +"%Y%m%d_%H%M%S_%N")
 
 ## now loop through the above array
 for a in `seq 1 $reps`; do
    arr[0]="experiments_mid_low/fsm/"`java -jar selectConfig.jar ./experiments_mid_low/fsm/configurations_fsm_15.txt 2`".txt"
-   arr[1]="experiments_mid_low/fsm/"`java -jar selectConfig.jar ./experiments_mid_low/fsm/configurations_fsm_15.txt 0.5`".txt"
-   arr[2]="experiments_mid_low/fsm/"`java -jar selectConfig.jar ./experiments_mid_low/fsm/configurations_fsm_15.txt 1.0`".txt"
-   for i in "${arr[@]}"; do
+   arr[1]="experiments_mid_low/fsm/"`java -jar selectConfig.jar ./experiments_mid_low/fsm/configurations_fsm_15.txt 176`".txt"
+   arr[2]="experiments_mid_low/fsm/"`java -jar selectConfig.jar ./experiments_mid_low/fsm/configurations_fsm_15.txt 352`".txt"
+
+   cp ${arr[0]} ./SULs/2feat.txt
+   cp ${arr[1]} ./SULs/50Perc.txt
+   cp ${arr[2]} ./SULs/100Perc.txt
+   
+   for i in "${arr_suls[@]}"; do
       echo java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
       java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
-      for j in "${arr[@]}"; do
+      for j in "${arr_suls[@]}"; do
          for b in `seq 1 $reps`; do
             java -jar ./Infer_LearnLib.jar -sul $j -ot $i.ot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
          done
@@ -52,8 +55,3 @@ sed -i "s/|\ /|/g" ./log4j/log.tab
 
 mkdir $logdir/
 mv ./log4j $logdir/
-mv ./experiments_mid_low/fsm/fsm_*.ot  $logdir/
-mv ./experiments_mid_low/fsm/fsm_*.sul  $logdir/
-mv ./experiments_mid_low/fsm/fsm_*.infer  $logdir/
-mv ./experiments_mid_low/fsm/fsm_*.final  $logdir/
-mv ./experiments_mid_low/fsm/fsm_*.reval  $logdir/
