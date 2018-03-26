c1d50b6081c22380a5b5df4032811c3574fda823
damascenodiego <carlos.damasceno08@gmail.com>
2018-03-26 20:02:14 -0300

    experiment v1.1

---
+++ mealyInference/runTest_random_dlstar.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# source constants.sh
+rnd_scens=10; num_revals=20
+## declare an array variable
+arr=("2.txt" "8.txt" "16.txt");
+arr_suls=("./SULs/2.txt" "./SULs/8.txt" "./SULs/16.txt")
+mkdir -p ./SULs
+
+rm ./log4j/*.log
+rm ./SULs/*
+
+logdir=log_experiments_random_dlstar$(date +"%Y%m%d_%H%M%S_%N")
+
+random_confs=("experiments_random/fsm/configurations_fsm_15_16.txt"
+"experiments_random/fsm/configurations_fsm_15_17.txt"
+"experiments_random/fsm/configurations_fsm_15_18.txt"
+"experiments_random/fsm/configurations_fsm_15_19.txt"
+"experiments_random/fsm/configurations_fsm_15_20.txt"
+"experiments_random/fsm/configurations_fsm_15_21.txt"
+"experiments_random/fsm/configurations_fsm_15_22.txt"
+"experiments_random/fsm/configurations_fsm_15_23.txt"
+"experiments_random/fsm/configurations_fsm_15_24.txt"
+"experiments_random/fsm/configurations_fsm_15_25.txt"
+"experiments_random/fsm/configurations_fsm_15_26.txt"
+"experiments_random/fsm/configurations_fsm_15_27.txt"
+"experiments_random/fsm/configurations_fsm_15_28.txt"
+"experiments_random/fsm/configurations_fsm_15_29.txt"
+"experiments_random/fsm/configurations_fsm_15_30.txt")
+
+for conf in "${random_confs[@]}"; do
+   ## now loop through the above array
+   for a in `seq 1 $rnd_scens`; do
+      arr[0]="experiments_random/fsm/"`java -jar selectConfig.jar ./$conf 2`".txt"
+      arr[1]="experiments_random/fsm/"`java -jar selectConfig.jar ./$conf 0.5`".txt"
+      arr[2]="experiments_random/fsm/"`java -jar selectConfig.jar ./$conf 1.0`".txt"
+
+      cp ${arr[0]} ./SULs/2.txt
+      cp ${arr[1]} ./SULs/8.txt
+      cp ${arr[2]} ./SULs/16.txt
+      
+      for i in "${arr_suls[@]}"; do
+         echo java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
+         java -jar ./Infer_LearnLib.jar -sul $i -sot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
+         for j in "${arr_suls[@]}"; do
+            for b in `seq 1 $num_revals`; do
+               java -jar ./Infer_LearnLib.jar -sul $j -ot $i.ot -cexh RivestSchapire -clos CloseFirst -cache -eq rndWalk
+            done
+         done
+      done
+   done
+done
+
+echo "SUL|Cache|Reuse|CloS|CExH|EqO|L_ms|Rounds|SCEx_ms|MQ_Resets|MQ_Symbols|EQ_Resets|EQ_Symbols|Correct" > log4j/log.tab
+for i in  ./log4j/logback*.log; do
+   line=`grep "|SUL name"  $i                                       | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Cache"  $i                               | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Reused OT:"  $i                          | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|ClosingStrategy: CloseFirst" $i          | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|ObservationTableCEXHandler:" $i          | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|EquivalenceOracle:"  $i                  | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Learning \[ms\]:"  $i                    | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Rounds:"  $i                             | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Searching for counterexample \[ms\]" $i  | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|MQ \[resets\]"  $i                       | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|MQ \[symbols\]" $i                       | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|EQ \[resets\]"  $i                       | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|EQ \[symbols\]" $i                       | cut -d\|  -f2- | cut -d:  -f2- `
+   line="${line}|"`grep "|Number of states: " $i                   | cut -d\|  -f2- | cut -d:  -f2- `
+   echo $line >> log4j/log.tab
+done
+sed -i "s/|\ /|/g" ./log4j/log.tab
+
+mkdir $logdir/
+mv ./log4j $logdir/
+
+Rscript ./plotcharts.r $logdir
---
+++ mealyInference/src/main/java/br/usp/icmc/labes/mealyInference/IrfanExample.java
@@ -0,0 +1,119 @@
+package br.usp.icmc.labes.mealyInference;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import br.usp.icmc.labes.mealyInference.utils.Utils;
+import de.learnlib.algorithms.lstar.ce.ObservationTableCEXHandler;
+import de.learnlib.algorithms.lstar.ce.ObservationTableCEXHandlers;
+import de.learnlib.algorithms.lstar.closing.ClosingStrategies;
+import de.learnlib.algorithms.lstar.closing.ClosingStrategy;
+import de.learnlib.algorithms.lstar.mealy.ExtensibleLStarMealy;
+import de.learnlib.algorithms.lstar.mealy.ExtensibleLStarMealyBuilder;
+import de.learnlib.api.SUL;
+import de.learnlib.api.oracle.EquivalenceOracle;
+import de.learnlib.api.oracle.MembershipOracle;
+import de.learnlib.api.query.DefaultQuery;
+import de.learnlib.api.statistic.StatisticSUL;
+import de.learnlib.datastructure.observationtable.writer.ObservationTableASCIIWriter;
+import de.learnlib.driver.util.MealySimulatorSUL;
+import de.learnlib.filter.cache.mealy.MealyCaches;
+import de.learnlib.filter.cache.sul.SULCaches;
+import de.learnlib.filter.statistic.sul.ResetCounterSUL;
+import de.learnlib.filter.statistic.sul.SymbolCounterSUL;
+import de.learnlib.oracle.equivalence.WpMethodEQOracle;
+import de.learnlib.oracle.equivalence.mealy.RandomWalkEQOracle;
+import de.learnlib.oracle.membership.SULOracle;
+import de.learnlib.util.statistics.SimpleProfiler;
+import net.automatalib.automata.transout.MealyMachine;
+import net.automatalib.automata.transout.impl.compact.CompactMealy;
+import net.automatalib.serialization.dot.GraphDOT;
+import net.automatalib.words.Word;
+
+public class IrfanExample {
+
+	public static void main(String[] args) {
+		
+		try {
+			// set SUL path
+			File sul = new File("./experiments_irfan/example.txt");
+						
+			// load mealy machine
+			CompactMealy<String, Word<String>> mealyss = Utils.getInstance().loadMealyMachine(sul);
+			
+			// set closing strategy
+			ClosingStrategy strategy 			= ClosingStrategies.CLOSE_FIRST;
+
+			// set CE processing approach
+			ObservationTableCEXHandler handler 	= ObservationTableCEXHandlers.RIVEST_SCHAPIRE;
+			
+			// Empty list of prefixes 
+			List<Word<String>> initPrefixes = new ArrayList<Word<String>>();
+			initPrefixes.add(Word.epsilon());
+						
+			// Empty list of suffixes => minimal compliant setinitCes
+			List<Word<String>> initSuffixes = new ArrayList<Word<String>>();
+
+
+			// SUL simulator
+			SUL<String,Word<String>> sulSim = new MealySimulatorSUL(mealyss, Utils.getInstance().OMEGA_SYMBOL);
+						
+			// membership oracle for counting queries wraps sul
+			StatisticSUL<String, Word<String>>  mqSul_sym = new SymbolCounterSUL<>("MQ", sulSim);
+			StatisticSUL<String, Word<String>>  mqSul_rst = new ResetCounterSUL <>("MQ", mqSul_sym);			
+			MembershipOracle<String, Word<Word<String>>> mqOracle = new SULOracle<String, Word<String>>(mqSul_rst);
+
+			// use caching to avoid duplicate queries
+			mqOracle = MealyCaches.createTreeCache(mealyss.getInputAlphabet(), mqOracle);
+
+
+			// construct L* instance 
+			ExtensibleLStarMealyBuilder<String, Word<String>> builder = new ExtensibleLStarMealyBuilder<String, Word<String>>();
+			builder.setAlphabet(mealyss.getInputAlphabet());
+			builder.setOracle(mqOracle);
+			builder.setInitialPrefixes(initPrefixes);
+			builder.setInitialSuffixes(initSuffixes);
+			builder.setCexHandler(handler);
+			builder.setClosingStrategy(strategy);
+
+			ExtensibleLStarMealy<String, Word<String>> learner = builder.create();
+			
+			// equivalence oracle for counting queries wraps sul
+			StatisticSUL<String, Word<String>> eqSul_sym = new SymbolCounterSUL<>("EQ", sulSim);
+			StatisticSUL<String, Word<String>> eqSul_rst = new ResetCounterSUL<>("EQ", eqSul_sym);
+			SUL<String, Word<String>> eq_sul = SULCaches.createTreeCache(mealyss.getInputAlphabet(), eqSul_rst);
+			MembershipOracle<String, Word<Word<String>>> sulEqOracle = new SULOracle<String, Word<String>>(eq_sul);
+			EquivalenceOracle eqOracle = new WpMethodEQOracle<>(sulEqOracle, 2);
+			
+			
+			new ObservationTableASCIIWriter<>().write(learner.getObservationTable(), System.out);
+			learner.startLearning();
+			new ObservationTableASCIIWriter<>().write(learner.getObservationTable(), System.out);
+			
+			Word<String> word = Word.epsilon();
+			word=word.append("a");
+			word=word.append("b");
+			word=word.append("a");
+			word=word.append("b");
+			word=word.append("a");
+			word=word.append("a");
+			word=word.append("b");
+			
+			System.out.println(word);
+			DefaultQuery<String, Word<Word<String>>> ce = new DefaultQuery<>(word);
+			ce.answer(mqOracle.answerQuery(word));
+			
+			learner.refineHypothesis(ce);
+				
+			new ObservationTableASCIIWriter<>().write(learner.getObservationTable(), System.out);
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
