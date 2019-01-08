48364f16e61a5af60b233ce4df91ca2be3030343
damascenodiego <damascenodiego@usp.br>
2019-01-08 17:49:41 +0000

    including <numberOfRepetitions> parameter to the info text

--- mealyInference/src/main/java/experiments/Info.java
+++ mealyInference/src/main/java/experiments/Info.java
@@ -5,13 +5,13 @@ public class Info {
 		System.err.println("Run one of the following commands:");
 		
 		System.out.println("java -cp runcomparison.jar experiments.br.usp.icmc.labes.ExperimentNordsec16CreateOTs");
-		System.out.println("java -cp java -cp runcomparison.jar experiments.br.usp.icmc.labes.ExperimentNordsec16 $var <numberOfRepetitions>");
+		System.out.println("java -cp java -cp runcomparison.jar experiments.br.usp.icmc.labes.ExperimentNordsec16 <numberOfRepetitions>");
 
 		
 		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentCreateOTs");
 		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentWMethod");
-		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethod");
-		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethodQsize");
+		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethod <numberOfRepetitions>");
+		System.out.println("java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethodQsize <numberOfRepetitions>");
 		
 
 		
