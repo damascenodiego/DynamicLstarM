b687dc66d2a2938d8743baf9b2b66f2a3c8bcd2a
damascenodiego <damascenodiego@usp.br>
2019-01-08 17:46:47 +0000

    info program to show options

---
+++ mealyInference/src/main/java/experiments/Info.java
@@ -0,0 +1,21 @@
+package experiments;
+
+public class Info {
+	public static void main(String[] args) {
+		System.err.println("Run one of the following commands:");
+		
+		System.err.println("\t java -cp runcomparison.jar experiments.br.usp.icmc.labes.ExperimentNordsec16CreateOTs");
+		System.err.println("\t java -cp java -cp runcomparison.jar experiments.br.usp.icmc.labes.ExperimentNordsec16 $var <numberOfRepetitions>");
+
+		
+		System.err.println("\t java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentCreateOTs");
+		System.err.println("\t java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentWMethod");
+		System.err.println("\t java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethod");
+		System.err.println("\t java -cp runcomparison.jar experiments.uk.ac.le.RunExperimentRandomWMethodQsize");
+		
+
+		
+
+		
+	}
+}
